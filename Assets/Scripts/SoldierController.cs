8f43ac18b50ac50d5825792e2e104ef61ee048af
David ROGER <david.roger15@gmail.com>
2015-08-22 11:54:26 +0200

    Added pawn and controller

---
+++ Assets/Scripts/SoldierController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SoldierController : MonoBehaviour {
+
+	private SoldierPawn soldierPawn;
+
+	void Start () {
+		soldierPawn = GetComponent<SoldierPawn> ();
+	}
+
+	void Update () {
+		if (!soldierPawn.isActing ()) {
+			Process();
+		}
+	}
+
+	string Process() {
+		return "Speak";
+	}
+}
---
+++ Assets/Scripts/SoldierPawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierPawn : MonoBehaviour {
+
+	bool act = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Speak() {
+		Debug.Log ("Oy Mate");
+	}
+
+	public bool isActing() {
+		return act;
+	}
+}
